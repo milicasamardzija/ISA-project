a03bf7563511091ad322d9dce493ff7076910d9e
nevenaatic <atic.ra67.2018@uns.ac.rs>
2021-11-25 22:50:01 +0100

    feat: registration form
    responsive

--- project-frontend/src/App.vue
+++ project-frontend/src/App.vue
@@ -1,19 +1,21 @@
 <template>
- <HeaderStartPage />
- 
+  <div>
+    <HeaderLogAndRegister/>
+    <HeaderStartPage/>
+  </div>
+
 </template>
 
 <script>
 import HeaderStartPage from "./components/HeaderStartPage.vue";
-
+import HeaderLogAndRegister from "./components/HeaderLogAndRegister.vue";
 export default {
   name: "App",
   components: {
     HeaderStartPage,
+    HeaderLogAndRegister,
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
--- project-frontend/src/components/HeaderStartPage.vue
+++ project-frontend/src/components/HeaderStartPage.vue
@@ -1,42 +1,35 @@
 <template>
-    <div id="demo" class="carousel slide" data-ride="carousel">
+  <div id="demo" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
 
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-  
-  <!-- The slideshow -->
-  <div class="carousel-inner" style="bacground-repeat:no repeat">
-    <div class="carousel-item active">
-      <img src="../assets/cottageStart.jpg" width="2200" height="500">
+    <!-- The slideshow -->
+    <div class="carousel-inner" style="bacground-repeat: no repeat">
+      <div class="carousel-item active">
+        <img src="../assets/cottageStart.jpg" width="2200" height="500" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/boatStart.jpg" width="2200" height="500" />
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/fishingStart.jpg" width="2200" height="500" />
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="../assets/boatStart.jpg" width="2200" height="500">
-    </div>
-    <div class="carousel-item ">
-      <img src="../assets/fishingStart.jpg" width="2200" height="500">
-    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
   </div>
-  
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-</div>
-
 </template>
 
-<script>
-   
-</script>
+<script></script>
 
-
-<style scoped>
-  
-</style>
+<style scoped></style>
