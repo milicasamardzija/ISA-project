e028f7d50e3822b20e3fc4924d14800fcd81e5ee
Milica <samardzija.ra66.2018@uns.ac.rs>
2021-12-07 01:55:00 +0100

    feat: model start

---
+++ project-backend/src/main/java/model/Address.java
@@ -0,0 +1,78 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "country", nullable = false)
+	private String country;
+	@Column(name = "city", nullable = false)
+	private String city;
+	@Column(name = "street", nullable = false)
+	private String street;
+	@Column(name = "number", nullable = false)
+	private String number;
+	
+	public Address() {}
+	
+	public Address(int id, String country, String city, String street, String number) {
+		super();
+		this.id = id;
+		this.country = country;
+		this.city = city;
+		this.street = street;
+		this.number = number;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+}
---
+++ project-backend/src/main/java/model/BoatOwner.java
@@ -0,0 +1,5 @@
+package model;
+
+public class BoatOwner extends User {
+
+}
