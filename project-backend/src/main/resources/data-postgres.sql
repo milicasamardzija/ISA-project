fc150de2b4bb8d344b7ec542dc67ed0e0c79c4d7
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-01-04 14:10:04 +0100

    feat: address added

--- project-backend/src/main/resources/data-postgres.sql
+++ project-backend/src/main/resources/data-postgres.sql
@@ -1,6 +1,9 @@
--- roles
 INSERT INTO role(id, name) VALUES (nextval('role_id_seq'), 'ROLE_CLIENT');
 INSERT INTO role(id, name) VALUES (nextval('role_id_seq'), 'ROLE_ADMIN');
 INSERT INTO role(id, name) VALUES (nextval('role_id_seq'), 'ROLE_COTTAGE_OWNER');
 INSERT INTO role(id, name) VALUES (nextval('role_id_seq'), 'ROLE_SHIP_OWNER');
 INSERT INTO role(id, name) VALUES (nextval('role_id_seq'), 'ROLE_INSTRUCTOR');
+
+INSERT INTO address(id, country, city, street, number) VALUES (nextval('address_id_seq'), 'Srbija', 'Sabac', 'Vojvode Misica', 5);
+INSERT INTO address(id, country, city, street, number) VALUES (nextval('address_id_seq'), 'Srbija', 'Novi Sad', 'Lilike Bem', 15);
+INSERT INTO address(id, country, city, street, number) VALUES (nextval('address_id_seq'), 'Srbija', 'Sabac', 'Masarikova', 3);
