92386c48a92672521585f6d17152459bad46d79f
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-06-06 15:27:25 +0200

    fix: email

--- project-frontend/src/views/client/entities/ClientAllAdventures.vue
+++ project-frontend/src/views/client/entities/ClientAllAdventures.vue
@@ -122,9 +122,12 @@
           <div class="col-with-picture">
             <div v-if="adventure.images.length != 0">
               <img
-                :src="getImgUrl(adventure.images[0].filePath)"
-                style="height: 250px !important; width: 300px !important" @click="goToAdventure()"
-              />
+              :src="cottage.images[0]"
+              class="rounded img-thumbnail img-fluid"
+              alt="Responsive image"
+              style="margin-top: 1rem; max-height: 300px !important; max-width: 320px !important"
+               @click="goToAdventure(adventure.nameOfAdventure),getSelected(adventure)"
+            />
             </div>
           </div>
           <div class="col-info">
--- project-frontend/src/views/client/entities/ClientAllBoats.vue
+++ project-frontend/src/views/client/entities/ClientAllBoats.vue
@@ -118,8 +118,11 @@
         <div class="col-with-picture">
           <div v-if="boat.images.length != 0">
             <img
-              :src="getImgUrl(boat.images[0].filePath)"
-              style="height: 250px !important; width: 300px !important"
+              :src="cottage.images[0]"
+              class="rounded img-thumbnail img-fluid"
+              alt="Responsive image"
+              style="margin-top: 1rem; max-height: 300px !important; max-width: 320px !important"
+               @click="showBoat(boat)"
             />
           </div>
         </div>
